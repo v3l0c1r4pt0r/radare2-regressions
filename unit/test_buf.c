a000be29dacc16d6393b5c893d74154b828550c5
xarkes <xarkes@users.noreply.github.com>
2019-01-05 10:51:02 +0100

    Fixed test_buf test (#1626)

--- unit/test_buf.c
+++ unit/test_buf.c
@@ -1,20 +1,31 @@
 #include <r_util.h>
+#include <stdlib.h>
 #include "minunit.h"
-
-#define FILENAME __FILE__
 
 bool test_r_buf_file_new() {
 	RBuffer *b;
 	char buffer[1024] = { 0 };
 	int r;
+	char filename[] = "r2-XXXXXX";
+	const char *content = "Something To\nSay Here..";
+	const int length = 23;
 
-	b = r_buf_new_file (FILENAME, 0);
+	// Prepare file
+	int fd = mkstemp (filename);
+	mu_assert_neq (fd, -1, "mkstemp failed...");
+	write (fd, content, length);
+	close (fd);
+
+	b = r_buf_new_file (filename, 0);
 	mu_assert_notnull (b, "r_buf_new_file failed");
 
-	r = r_buf_read_at (b, R_BUF_CUR, buffer, 20);
-	mu_assert_eq (r, 20, "r_buf_read_at failed");
+	r = r_buf_read_at (b, R_BUF_CUR, buffer, length);
+	mu_assert_eq (r, length, "r_buf_read_at failed");
 
-	mu_assert_streq (buffer, "#include <r_util.h>\n", "r_buf_read_at has corrupted content");
+	mu_assert_streq (buffer, content, "r_buf_read_at has corrupted content");
+
+	// Cleanup
+	unlink (filename);
 
 	mu_end;
 }
