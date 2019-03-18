7581f5e4f291f0bb85dccd84c9e89d51d85c0f29
GustavoLCR <gugulcr@gmail.com>
2019-03-18 15:35:22 -0300

    Add unit tests for str_utf8_charsize (#1748)

--- unit/test_str.c
+++ unit/test_str.c
@@ -242,6 +242,50 @@ bool test_r_str_len_utf8_ansi(void) {
 	mu_end;
 }
 
+bool test_r_str_utf8_charsize(void) {
+	char s[16] = "\x61\xc3\xa1\xe6\x97\xa5\xf0\x9f\x91\x8c\xf0\x9f\x91\x8c\x8c"; // aá日👌
+	int sz;
+
+	sz = r_str_utf8_charsize (s);
+	mu_assert_eq (sz, 1, "1 byte UTF-8");
+
+	sz = r_str_utf8_charsize (s + 1);
+	mu_assert_eq (sz, 2, "2 byte UTF-8");
+
+	sz = r_str_utf8_charsize (s + 3);
+	mu_assert_eq (sz, 3, "3 byte UTF-8");
+
+	sz = r_str_utf8_charsize (s + 6);
+	mu_assert_eq (sz, 4, "4 byte UTF-8");
+
+	sz = r_str_utf8_charsize (s + 10);
+	mu_assert_eq (sz, 0, "Malformed UTF-8");
+
+	mu_end;
+}
+
+bool test_r_str_utf8_charsize_prev(void) {
+	char s[16] = "\x61\xc3\xa1\xe6\x97\xa5\xf0\x9f\x91\x8c\xf0\x9f\x91\x8c\x8c"; // aá日👌
+	int sz;
+
+	sz = r_str_utf8_charsize_last (s);
+	mu_assert_eq (sz, 0, "Malformed UTF-8");
+
+	sz = r_str_utf8_charsize_prev (s + 10, 10);
+	mu_assert_eq (sz, 4, "4 byte UTF-8");
+
+	sz = r_str_utf8_charsize_prev (s + 6, 6);
+	mu_assert_eq (sz, 3, "3 byte UTF-8");
+
+	sz = r_str_utf8_charsize_prev (s + 3, 3);
+	mu_assert_eq (sz, 2, "2 byte UTF-8");
+
+	sz = r_str_utf8_charsize_prev (s + 1, 1);
+	mu_assert_eq (sz, 1, "1 byte UTF-8");
+
+	mu_end;
+}
+
 bool test_r_str_sanitize_sdb_key(void) {
 	char *s = r_str_sanitize_sdb_key("rada.re2<is>::Cool");
 	mu_assert_streq (s, "rada_re2_is_::Cool", "sanitize");
@@ -293,6 +337,8 @@ bool all_tests() {
 	mu_run_test(test_r_str_rchr);
 	mu_run_test(test_r_str_ansi_len);
 	mu_run_test(test_r_str_len_utf8_ansi);
+	mu_run_test(test_r_str_utf8_charsize);
+	mu_run_test(test_r_str_utf8_charsize_prev);
 	mu_run_test(test_r_str_sanitize_sdb_key);
 	mu_run_test(test_r_str_unescape);
 	return tests_passed != tests_run;
