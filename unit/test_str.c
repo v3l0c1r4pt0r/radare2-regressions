d2ff149aa0385d7e370148ebf56bba3d77d63c34
Florian Märkl <info@florianmaerkl.de>
2019-11-06 17:54:29 +0100

    Add Unit Test for RStrConstPool

--- unit/test_str.c
+++ unit/test_str.c
@@ -344,6 +344,31 @@ bool test_r_str_newf(void) {
 	mu_end;
 }
 
+bool test_r_str_constpool(void) {
+	RStrConstPool pool;
+	bool s = r_str_constpool_init (&pool);
+	mu_assert ("pool init success", s);
+
+	const char *a_ref = "deliverance";
+	const char *a_pooled = r_str_constpool_get (&pool, a_ref);
+	mu_assert_ptrneq (a_pooled, a_ref, "pooled != ref");
+	mu_assert_streq (a_pooled, a_ref, "pooled == ref (strcmp)");
+	const char *a_pooled2 = r_str_constpool_get (&pool, a_ref);
+	mu_assert_ptreq (a_pooled2, a_pooled, "same on re-get");
+	char *a_ref_cpy = strdup (a_ref);
+	a_pooled2 = r_str_constpool_get (&pool, a_ref_cpy);
+	free (a_ref_cpy);
+	mu_assert_ptreq (a_pooled2, a_pooled, "same on re-get with different ptr");
+
+	const char *b_ref = "damnation";
+	const char *b_pooled = r_str_constpool_get (&pool, b_ref);
+	mu_assert_ptrneq (b_pooled, b_ref, "pooled != ref (second)");
+	mu_assert_streq (b_pooled, b_ref, "pooled == ref (strcmp, second)");
+
+	r_str_constpool_fini (&pool);
+	mu_end;
+}
+
 bool all_tests() {
 	mu_run_test(test_r_str_newf);
 	mu_run_test(test_r_str_replace_char_once);
@@ -369,6 +394,7 @@ bool all_tests() {
 	mu_run_test(test_r_str_utf8_charsize_prev);
 	mu_run_test(test_r_str_sanitize_sdb_key);
 	mu_run_test(test_r_str_unescape);
+	mu_run_test(test_r_str_constpool);
 	return tests_passed != tests_run;
 }
 
